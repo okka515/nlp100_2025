53f41808ea1b95fc6b22f3f8ec2e854a797e2e00
okka515 <okaaya734@gmail.com>
2026-04-27 18:13:20 +0900

    add: 解説コメントアウト

--- ch01/1_09.py
+++ ch01/1_09.py
@@ -12,3 +12,16 @@ def typoglycemia(s: str) -> str:
 
 s = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 print(typoglycemia(s))
+
+'''
+＃09
+英文をスペースで分割、各単語を引数としてshuffle_word()へ
+
+語長が5以上の場合、先頭と末尾を除いた箇所をシャッフル
+  ・-1は末尾
+  ・pythonにおける文字列は変更不可能（イミュータブル）のため
+  　random.shuffle()関数の利用にはlist()による文字配列変換が必要
+  ・random.shuffle()関数の戻り値は None であるため、
+  　まるごとjoin関数の引数に入れるとエラーが出るので注意
+　・先頭, 間(リスト→文字列変換後), 末尾を結合
+'''
